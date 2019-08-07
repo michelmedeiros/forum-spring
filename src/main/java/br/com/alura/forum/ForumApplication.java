695c81ae5f182de5a0eb640316e23b0e3cf85fab
michelmedeiros <michel.tds@gmail.com>
2019-08-07 06:11:53 -0300

    Add spring boot admin

--- src/main/java/br/com/alura/forum/ForumApplication.java
+++ src/main/java/br/com/alura/forum/ForumApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport
@@ -11,6 +12,8 @@ import org.springframework.data.web.config.EnableSpringDataWebSupport;
 public class ForumApplication {
 
 	public static void main(String[] args) {
+		BCryptPasswordEncoder cript = new BCryptPasswordEncoder();
+		System.out.println(cript.encode("mc007741").toString());
 		SpringApplication.run(ForumApplication.class, args);
 	}
 
